429bd275592e544ae001100222e3551ca0033d12
=marwa <=marwajawad19@gmail>
2023-06-03 11:50:06 -0700

    store data in localstorage

--- src/stores/api.js
+++ src/stores/api.js
@@ -5,11 +5,16 @@ export const addProduct = (data) => {
   axios
     .post(`${URL}/products`, data)
     .then((response) => {
-      console.log(response.json())
+      //  console.log(response.json())
     })
     .catch((error) => {
       console.log(error)
     })
+  // update localstorage
+  let updatedStore = JSON.parse(localStorage.getItem('items'))
+  updatedStore.unshift(data)
+  console.log(data, 'in the api adding', updatedStore)
+  localStorage.setItem('items', JSON.stringify(updatedStore))
 }
 // get all products
 export const getProducts = (data) => {
@@ -28,9 +33,38 @@ export const deletProduct = (id) => {
   axios
     .delete(`${URL}/products/${id}`)
     .then((response) => {
-      console.log(response.data, 'delete it ')
+      // console.log(response.data, 'delete it ')
     })
     .catch((error) => {
       console.log(error)
     })
+
+  // update localstorage
+  let updatedStore = JSON.parse(localStorage.getItem('items'))
+  updatedStore = updatedStore?.filter((item) => item.id !== id)
+  localStorage.setItem('items', JSON.stringify(updatedStore))
+  window.location.reload()
+  return updatedStore
+}
+
+// updateProduct
+
+export const updateAPI = (id, data) => {
+  axios
+    .put(`${URL}/products/${id}`, data)
+    .then((response) => {})
+    .catch((error) => {
+      console.log(error)
+    })
+  // update localstorage
+
+  let updatedStore = JSON.parse(localStorage.getItem('items'))
+  updatedStore = updatedStore.map((obj) => {
+    if (obj.id === id) {
+      return { ...obj, ...data }
+    }
+    return obj
+  })
+
+  localStorage.setItem('items', JSON.stringify(updatedStore))
 }
