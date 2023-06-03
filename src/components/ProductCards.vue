8600da32ab65cf933f2be0079260009ab5ab462c
=marwa <=marwajawad19@gmail>
2023-06-03 00:05:03 -0700

    adding image to the card

--- src/components/ProductCards.vue
+++ src/components/ProductCards.vue
@@ -1,11 +1,11 @@
 <template>
   <v-row>
     <v-col v-for="(item, index) in items" :key="index" cols="12" sm="6" md="4">
-      <v-card>
-        <v-card-media :src="item.image"></v-card-media>
+      <v-card :height="400">
+        <v-img :src="item.image" :width="200" :height="200"></v-img>
 
         <v-card-title>{{ item.title }}</v-card-title>
-        <v-card-text>{{ item.description }}</v-card-text>
+        <v-card-text :height="50">{{ item.description }}</v-card-text>
         <v-card-actions>
           <span class="mr-3">{{ item.price }}</span>
           <span>{{ item.category }}</span>
