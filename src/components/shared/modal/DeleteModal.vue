5cdddd24f483b581f7dfde4e643d58c083ba31b7
=marwa <=marwajawad19@gmail>
2023-06-04 01:15:37 -0700

    create modal for delete popup modal

---
+++ src/components/shared/modal/DeleteModal.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="text-center">
+    <v-dialog v-model="dialog" width="auto">
+      <template v-slot:activator="{ props }">
+        <p v-bind="props">Delete</p>
+      </template>
+
+      <v-card>
+        <v-card-title> Delete Item</v-card-title>
+        <v-card-text>Are you sure you want to delete this item?</v-card-text>
+
+        <v-card-actions>
+          <v-btn color="green darken-1" text @click="handleDelete">Confirm</v-btn>
+          <v-btn color="red darken-1" text @click="dialog = false">Cancel</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+  </div>
+</template>
+<script setup>
+defineProps({
+  onDelete: {
+    type: Function,
+    required: true
+  }
+})
+</script>
+<script>
+export default {
+  methods: {
+    handleDelete() {
+      this.dialog = false
+      this.onDelete()
+    }
+  },
+  data() {
+    return {
+      dialog: false
+    }
+  }
+}
+</script>
+
+<style>
+.text-center {
+  text-align: center;
+}
+</style>
