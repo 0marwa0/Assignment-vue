429a366a9b871e34848c7a27e6dce4f15ff58417
=marwa <=marwajawad19@gmail>
2023-06-04 01:15:05 -0700

    create input text component

---
+++ src/components/shared/inputText.vue
@@ -0,0 +1,24 @@
+<template>
+  <label for="Title">{{ lable }}:</label>
+
+  <v-text-field
+    :id="lable"
+    clearable
+    variant="underlined"
+    :value="value"
+    @input="handleChange($event, lable)"
+  ></v-text-field>
+</template>
+<script setup>
+defineProps({
+  lable: {
+    type: String,
+    required: true
+  },
+  value: {
+    type: String,
+    required: true
+  },
+  handleChange: { type: Function, required: true }
+})
+</script>
